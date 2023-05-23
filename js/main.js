6379341467db960ec008d9f14d8dea3fa872e486
InnaPopenko <inusyk0987@icloud.com>
2023-05-23 08:27:08 +0200

    adding js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
+ 
+//1. Перевірка темної теми на рівні системних налаштувань 
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
 
+//2. Перевірка темної теми localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+//Якщо міняються системні налаштування мінняється тема
+window
+.matchMedia("(prefers-color-scheme: dark)")
+.addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if (newColorScheme === "dark") {
+        btnDarkMode.classList.add("dark-mode-btn--active");
+        document.body.classList.add("dark");
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        btnDarkMode.classList.remove("dark-mode-btn--active");
+        document.body.classList.remove("dark");
+        localStorage.setItem("darkMode", "light");
+    }
+});
+
+//Включання нічного режиму
 btnDarkMode.onclick = function () {
-    console.log('onclick')
     btnDarkMode.classList.toggle("dark-mode-btn--active");
-} 
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+}; 
+
+
+
